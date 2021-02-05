2449bd3a54c7f863afaba1376a7978ad1b030e19
Robert Kania <cezaryrk@yahoo.de>
2021-02-05 14:04:46 +0100

    refactor: for ts 4.1.3

--- src/browser.ts
+++ src/browser.ts
@@ -1,25 +1,25 @@
-export * from './utils/INpmlsOptions';
+export {INpmlsOptions} from './utils/INpmlsOptions';
 
-export * from './registry/ICache';
-export * from './registry/ISubModule';
-export * from './registry/IModuleRegistryOptions';
-export * from './registry/ModuleDescriptor';
-export * from './registry/IModuleRegistry';
+export {ICache} from './registry/ICache';
+export {ISubModule} from './registry/ISubModule';
+export {IModuleRegistryOptions} from './registry/IModuleRegistryOptions';
+export {ModuleDescriptor} from './registry/ModuleDescriptor';
+export {IModuleRegistry} from './registry/IModuleRegistry';
 
-export * from './loader/IModuleLoader';
-export * from './loader/AbstractModuleHandle';
-export * from './loader/AbstractModuleLoader';
-export * from './loader/IModuleOptions';
+export {IModuleLoader} from './loader/IModuleLoader';
+export {AbstractModuleHandle} from './loader/AbstractModuleHandle';
+export {AbstractModuleLoader} from './loader/AbstractModuleLoader';
+export {IModuleOptions} from './loader/IModuleOptions';
 
-export * from './loader/require/IRequireOptions';
-export * from './loader/require/RequireHandle';
-export * from './loader/require/IRequireLoader';
+export {IRequireOptions} from './loader/require/IRequireOptions';
+export {RequireHandle} from './loader/require/RequireHandle';
+export {IRequireLoader} from './loader/require/IRequireLoader';
 
-export * from './loader/classes/IClassesOptions';
-export * from './loader/classes/ClassesHandle';
-export * from './loader/classes/IClassesLoader';
+export {IClassesOptions} from './loader/classes/IClassesOptions';
+export {ClassesHandle} from './loader/classes/ClassesHandle';
+export {IClassesLoader} from './loader/classes/IClassesLoader';
 
-export * from './loader/settings/ISettingsOptions';
-export * from './loader/settings/SettingsHandle';
-export * from './loader/settings/ISettingsLoader';
+export {ISettingsOptions} from './loader/settings/ISettingsOptions';
+export {SettingsHandle} from './loader/settings/SettingsHandle';
+export {ISettingsLoader} from './loader/settings/ISettingsLoader';
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
-export * from './browser';
+export {
+  ISettingsLoader,
+  SettingsHandle,
+  ISettingsOptions,
+  IClassesLoader,
+  ClassesHandle,
+  IClassesOptions,
+  IRequireLoader,
+  RequireHandle,
+  IRequireOptions,
+  IModuleOptions,
+  AbstractModuleLoader,
+  AbstractModuleHandle,
+  IModuleLoader,
+  IModuleRegistry,
+  ModuleDescriptor,
+  IModuleRegistryOptions,
+  ISubModule,
+  ICache,
+  INpmlsOptions
+} from './browser';
 
-export * from './registry/ModuleRegistry';
-export * from './loader/require/RequireLoader';
-export * from './loader/settings/SettingsLoader';
-export * from './loader/classes/ClassesLoader';
+export {ModuleRegistry} from './registry/ModuleRegistry';
+export {RequireLoader} from './loader/require/RequireLoader';
+export {SettingsLoader} from './loader/settings/SettingsLoader';
+export {ClassesLoader} from './loader/classes/ClassesLoader';
